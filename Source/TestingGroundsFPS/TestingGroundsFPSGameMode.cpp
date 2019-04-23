0eb2cf39e93a84797482de16cb6468363fd351c8
Michael Ohl <mikeohl@live.com>
2019-04-23 01:20:42 -0700

    Reordered directories to align with design

--- Source/TestingGroundsFPS/TestingGroundsFPSGameMode.cpp
+++ Source/TestingGroundsFPS/TestingGroundsFPSGameMode.cpp
@@ -9,7 +9,7 @@ ATestingGroundsFPSGameMode::ATestingGroundsFPSGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviors/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
